b6e1d960508e3ee5e43bead33bd904d232eedfe7
BytteM3 <mateo666@live.com>
2025-07-11 11:45:57 +0200

    Added --verbose feature

--- main.py
+++ main.py
@@ -14,24 +14,27 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=sys.argv[1])])
-    ]
-
     if len(sys.argv) < 2:
         print("Please provide input!")
         sys.exit(1)
     
     else:
+        messages = [
+        types.Content(role="user", parts=[types.Part(text=sys.argv[1])])
+    ]
         result = client.models.generate_content(
             model="gemini-2.0-flash-001", contents=messages)
     
-    
     prompt_tokens = result.usage_metadata.prompt_token_count
     response_tokens = result.usage_metadata.candidates_token_count
-    print(result.text)
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
+
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {sys.argv[1]}")
+        print(result.text)
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    else:
+        print(result.text)
 
 if __name__ == "__main__":
     main()
